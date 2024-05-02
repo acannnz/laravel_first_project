cbb70114d0b86fc471222a7b081114f12be87026
Candra Pratama <putu.candra689@gmail.com>
2024-05-02 09:29:18 +0800

    label chart home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,7 +62,7 @@
                 <div class="col-md-12">
                     <div class="card card-danger">
                         <div class="card-header">
-                            <h3 class="card-title">Grafik Classroom</h3>
+                            <h3 class="card-title">Classroom Chart</h3>
 
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse">
